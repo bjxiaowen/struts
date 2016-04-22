8f4de31e2e869a9a3faf0e6f3875ecdf76c8e89d
bjxiaowen <bjxiaowen@126.com>
2016-04-22 12:05:42 +0800

    添加了测试类

---
+++ src/main/java/com/struts/web/Welcome.java
@@ -0,0 +1,19 @@
+package com.struts.web;
+
+public class Welcome {
+
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	// all struts logic here
+	public String execute() {
+		return "SUCCESS";
+	}
+}
